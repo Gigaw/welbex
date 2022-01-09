41ecca82bbd7fcfe6b15b675712775f332b2b32e
Гиголаев Игорь <gigolaev.i@introvert.bz>
2022-01-09 18:13:13 +0300

    связка react + express

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const cors = require('cors');
-const PORT = 8080
-const app = express()
-const rowRouter = require('./routes/row.routes')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORT = 8080;
+const app = express();
+const rowRouter = require("./routes/row.routes");
 
 app.use(cors());
-app.use(express.json())
-app.use('/api', rowRouter)
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.use(express.json());
+app.use("/api", rowRouter);
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
